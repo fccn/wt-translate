8c083b5dea766f918aeaffe6ca770d98de5addf4
development server <stv@asa.fccn.pt>
2017-10-25 10:08:33 +0100

    added localization extension to twig config

--- src/TranslateConfigurationLoader.php
+++ src/TranslateConfigurationLoader.php
@@ -19,6 +19,8 @@ class TranslateConfigurationLoader
   {
     //i18n extension
     $twig->addExtension(new \Twig_Extensions_Extension_I18n());
+    //localization extension
+    $twig->addExtension(new Twig_Extensions_Extension_Intl());
     $filter = new \Twig_SimpleFilter("translate", function($stdClassObject) {
       return null;
     });
